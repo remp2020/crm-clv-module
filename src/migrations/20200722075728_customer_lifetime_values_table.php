0323c817e9e042cd7cdcadec326b07f461f92558
Peter Dulacka <dulacka@gmail.com>
2023-12-17 15:14:04 +0100

    Update phinx, psr/log and tomaj/hermes
    - These libraries needed to be update together as they
    share the requirement to upgrade psr/log.
    - The robmorgan/phinx is now only required in application-module
    to remove the duplication with main composer.json.
    - Precision is now strictly enforced as integer, previously
    used string was causing errors.
    - Call getQueryBuilder() now requires an argument with
    the operation type. Affected places were updated.
    - Added annotations to the places where they were required
    by phpstan.
    
    remp/crm#3050

--- src/migrations/20200722075728_customer_lifetime_values_table.php
+++ src/migrations/20200722075728_customer_lifetime_values_table.php
@@ -11,12 +11,12 @@ class CustomerLifetimeValuesTable extends AbstractMigration
             ->addColumn('period_start', 'datetime', ['null' => false])
             ->addColumn('period_end', 'datetime', ['null' => false])
 
-            ->addColumn('period_amount', 'decimal', ['scale' => 2, 'precision' => '10', 'null' => false])
-            ->addColumn('percentile_0_amount', 'decimal', ['scale' => 2, 'precision' => '10', 'null' => false])
-            ->addColumn('percentile_25_amount', 'decimal', ['scale' => 2, 'precision' => '10', 'null' => false])
-            ->addColumn('percentile_50_amount', 'decimal', ['scale' => 2, 'precision' => '10', 'null' => false])
-            ->addColumn('percentile_75_amount', 'decimal', ['scale' => 2, 'precision' => '10', 'null' => false])
-            ->addColumn('percentile_100_amount', 'decimal', ['scale' => 2, 'precision' => '10', 'null' => false])
+            ->addColumn('period_amount', 'decimal', ['scale' => 2, 'precision' => 10, 'null' => false])
+            ->addColumn('percentile_0_amount', 'decimal', ['scale' => 2, 'precision' => 10, 'null' => false])
+            ->addColumn('percentile_25_amount', 'decimal', ['scale' => 2, 'precision' => 10, 'null' => false])
+            ->addColumn('percentile_50_amount', 'decimal', ['scale' => 2, 'precision' => 10, 'null' => false])
+            ->addColumn('percentile_75_amount', 'decimal', ['scale' => 2, 'precision' => 10, 'null' => false])
+            ->addColumn('percentile_100_amount', 'decimal', ['scale' => 2, 'precision' => 10, 'null' => false])
 
             ->addColumn('created_at', 'datetime', ['null' => false])
             ->addColumn('updated_at', 'datetime', ['null' => false])
